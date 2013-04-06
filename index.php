1a56db90674e4fd5d7a909bfd521020fb2ede021
Daniel Vassilev <vassda10@aol.com>
2013-04-06 16:48:06 +1100

    Added FontAwesome. Added an idea for index page design.

---
+++ index.php
@@ -0,0 +1,186 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>MCTrakr</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Le styles -->
+    <link href="./assets/css/bootstrap.css" rel="stylesheet">
+    <link href="./assets/css/font-awesome.min.css" rel="stylesheet">
+    <style type="text/css">
+      body {
+        padding-top: 40px;
+        padding-bottom: 60px;
+      }
+
+      /* Custom container */
+      .container {
+        margin: 0 auto;
+        max-width: 1000px;
+      }
+      .container > hr {
+        margin: 60px 0;
+      }
+
+      /* Main marketing message and sign up button */
+      .jumbotron {
+        margin: 80px 0;
+        text-align: center;
+      }
+      .jumbotron h1 {
+        font-size: 100px;
+        line-height: 1;
+      }
+      .jumbotron .lead {
+        font-size: 24px;
+        line-height: 1.25;
+      }
+      .jumbotron .btn {
+        font-size: 21px;
+        padding: 14px 24px;
+      }
+
+      /* Supporting marketing content */
+      .marketing {
+        margin: 60px 0;
+      }
+      .marketing p + h4 {
+        margin-top: 28px;
+      }
+
+
+      /* Customize the navbar links to be fill the entire space of the .navbar
+      .navbar .navbar-inner {
+        padding: 0;
+      }
+      .navbar .nav {
+        margin: 0;
+        display: table;
+        width: 100%;
+      }
+      .navbar .nav li {
+        display: table-cell;
+        width: 1%;
+        float: none;
+      }
+      .navbar .nav li a {
+        font-weight: bold;
+        text-align: center;
+        border-left: 1px solid rgba(255,255,255,.75);
+        border-right: 1px solid rgba(0,0,0,.1);
+      }
+      .navbar .nav li:first-child a {
+        border-left: 0;
+        border-radius: 3px 0 0 3px;
+      }
+      .navbar .nav li:last-child a {
+        border-right: 0;
+        border-radius: 0 3px 3px 0;
+      }*/
+    </style>
+    <link href="./assets/css/bootstrap-responsive.css" rel="stylesheet">
+
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="./assets/js/html5shiv.js"></script>
+    <![endif]-->
+
+    <!-- Fav and touch icons -->
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="./assets/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="./assets/ico/apple-touch-icon-114-precomposed.png">
+      <link rel="apple-touch-icon-precomposed" sizes="72x72" href="./assets/ico/apple-touch-icon-72-precomposed.png">
+                    <link rel="apple-touch-icon-precomposed" href="./assets/ico/apple-touch-icon-57-precomposed.png">
+                                   <link rel="shortcut icon" href="">
+  </head>
+
+  <body>
+  <div class="navbar navbar-fixed-top">
+      <div class="navbar-inner">
+          <div class="container">
+              <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+              </button>
+              <a class="brand" href="#">MCTrackr</a>
+              <div class="nav-collapse collapse">
+                  <ul class="nav">
+                      <li class="active"><a href="#"><i class="icon-home"></i> Home</a></li>
+                      <li><a href="#about"><i class="icon-book"></i> About</a></li>
+                      <li><a href="#contact"><i class="icon-bullhorn"></i> Contact</a></li>
+                      <li><a href="#downloads"><i class="icon-download"></i> Downloads</a></li>
+                  </ul>
+                  <ul class="nav pull-right">
+                      <li><a href="#dashboard"><i class="icon-dashboard"></i> Member Dashboard</a></li>
+                  </ul>
+              </div><!--/.nav-collapse -->
+          </div>
+      </div>
+  </div>
+
+  <div class="container">
+
+
+
+      <!-- Jumbotron -->
+      <div class="jumbotron">
+        <h1><img src="./assets/img/logo.png" alt="Statistics"/></h1><br/>
+        <p class="lead">We offer the most in-depth and accurate statistics for your minecraft server. Don't miss out! Just install the plugin and register</p>
+        <a class="btn btn-large btn-success" href="./register.php">Get started today</a>
+      </div>
+
+      <hr>
+
+      <!-- Example row of columns -->
+      <div class="row-fluid">
+        <div class="span4">
+          <center><h1><i class="icon-leaf icon-4x"></i></h1>
+          <h2>Lightweight</h2>
+          <p>Our Statistics plugin is incredibly powerful, fast and lightweight! It will run with ease on your server no matter the size. </p>
+          </center>
+        </div>
+
+        <div class="span4">
+            <center><h1><i class="icon-fighter-jet icon-4x"></i></h1>
+                <h2>Instant</h2>
+          <p>With instantly updating statistics, you and your players will never get left behind! Check out your profile page and watch those numbers climb. </p>
+          </center>
+       </div>
+        <div class="span4">
+            <center><h1><i class="icon-refresh icon-spin icon-4x"></i></h1>
+                <h2>24 / 7</h2>
+          <p>Our servers don't shutdown, we're here for you when you need us. Your players will be able to access their personal stats any time of the day.</p>
+          </center>
+        </div>
+      </div>
+
+      <hr>
+
+      <div class="footer">
+        <p>&copy; MCTrackr 2013</p>
+      </div>
+
+    </div> <!-- /container -->
+
+    <!-- Le javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="./assets/js/jquery.js"></script>
+    <script src="./assets/js/bootstrap-transition.js"></script>
+    <script src="./assets/js/bootstrap-alert.js"></script>
+    <script src="./assets/js/bootstrap-modal.js"></script>
+    <script src="./assets/js/bootstrap-dropdown.js"></script>
+    <script src="./assets/js/bootstrap-scrollspy.js"></script>
+    <script src="./assets/js/bootstrap-tab.js"></script>
+    <script src="./assets/js/bootstrap-tooltip.js"></script>
+    <script src="./assets/js/bootstrap-popover.js"></script>
+    <script src="./assets/js/bootstrap-button.js"></script>
+    <script src="./assets/js/bootstrap-collapse.js"></script>
+    <script src="./assets/js/bootstrap-carousel.js"></script>
+    <script src="./assets/js/bootstrap-typeahead.js"></script>
+
+  </body>
+</html>
